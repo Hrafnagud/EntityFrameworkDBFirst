ee5eab62b797139bf15305cd0ba87960051a4480
Emre Ç <superbevent@hotmail.com>
2021-12-24 15:10:20 +0300

    Add project files.

---
+++ EntityFrameworkDBFirst_BLL/ProductManager.cs
@@ -0,0 +1,36 @@
+﻿using EntityFrameworkDBFirst_BLL.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFrameworkDBFirstDAL;
+
+namespace EntityFrameworkDBFirst_BLL
+{
+    public class ProductManager
+    {
+        //Global
+        NORTHWNDEntities myDBContext = new NORTHWNDEntities();
+
+        public List<ProductViewModel> BringAllProducts()
+        {
+            List<ProductViewModel> products = new List<ProductViewModel>();
+            var data = myDBContext.Products;
+
+            foreach (var item in data)
+            {
+                ProductViewModel p = new ProductViewModel()
+                {
+                    CategoryID = item.CategoryID,
+                    ProductID = item.ProductID,
+                    UnitPrice = item.UnitPrice,
+                    UnitsInStock = item.UnitsInStock,
+                    Discontinued = item.Discontinued
+                };
+            }
+            return products;
+        }
+        
+    }
+}
