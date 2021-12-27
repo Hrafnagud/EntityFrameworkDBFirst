881bb5267e68b1263744830f8a107c71afe21f4b
Emre Ç <superbevent@hotmail.com>
2021-12-27 09:33:47 +0300

    dataGridView added. Testing for adding new product to Products table implemented (just for testing purpose.)

--- EntityFrameWorkDBFirstWinFormUI/Form1.cs
+++ EntityFrameWorkDBFirstWinFormUI/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EntityFrameworkDBFirst_BLL;
+using EntityFrameworkDBFirst_BLL.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,12 +11,41 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EntityFrameWorkDBFirstWinFormUI
-{
+{   //INSTALL ENTITYFRAMEWORK FOR UI AS WELL. COPY CONNECTIONSTRING FROM DAL's AppConfig and paste it into UI's AppConfig (inside configuration tags)
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+        //Global
+        ProductManager myProductManager = new ProductManager();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Let's bring product information into form load page.
+            BringProductsToGrid();
+        }
+
+        private void BringProductsToGrid()
+        {
+            //Bring list through BLL.
+            dataGridView1.DataSource = myProductManager.BringAllProducts();
+            dataGridView1.Columns["SupplierID"].Visible = false;
+            dataGridView1.Columns["CategoryID"].Visible = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //testing purpose. That's actually it for dbfirst
+            ProductViewModel newProduct = new ProductViewModel()
+            {
+                ProductName = "Testing Product",
+                Discontinued = false,
+                CategoryID = 1
+            };
+            myProductManager.AddNewProduct(newProduct);
+            //Method added to Product manager in a primitive manner. Just to test adding new item in a shallow implementation conventions.
         }
     }
 }
--- EntityFrameworkDBFirst_BLL/ProductManager.cs
+++ EntityFrameworkDBFirst_BLL/ProductManager.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EntityFrameworkDBFirstDAL;
-
 namespace EntityFrameworkDBFirst_BLL
 {
     public class ProductManager
@@ -26,11 +25,42 @@ namespace EntityFrameworkDBFirst_BLL
                     ProductID = item.ProductID,
                     UnitPrice = item.UnitPrice,
                     UnitsInStock = item.UnitsInStock,
-                    Discontinued = item.Discontinued
+                    Discontinued = item.Discontinued,
+                    UnitsOnOrder = item.UnitsOnOrder,
+                    ReorderLevel = item.ReorderLevel,
+                    SupplierID = item.SupplierID,
+                    ProductName = item.ProductName,
+                    QuantityPerUnit = item.QuantityPerUnit
                 };
+                //CategoryName
+                //First approach
+                p.CategoryName = item.Category.CategoryName;
+                //Second Approach
+                //p.CategoryName = myDBContext.Categories.FirstOrDefault(x => x.CategoryID == item.CategoryID)?.CategoryName;
+                products.Add(p);
             }
             return products;
         }
-        
+
+        public bool AddNewProduct(ProductViewModel p)
+        {
+            bool result = false;
+            Product newProduct = new Product
+            {
+                ProductName = p.ProductName,
+                Discontinued = p.Discontinued,
+                CategoryID = p.CategoryID
+            };
+
+            myDBContext.Products.Add(newProduct);
+            int affectedRows = myDBContext.SaveChanges();
+            if (affectedRows > 0)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
     }
 }
